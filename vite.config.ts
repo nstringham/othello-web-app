3362c360ce177f010c7e32238edb4775a0d16334
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-06-19 09:37:49 -0400

    settings button

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 import { VitePWA } from "vite-plugin-pwa";
 
+import * as mdi from "@mdi/js";
 import { manifest } from "./manifest";
 
 export default defineConfig({
   plugins: [
-    createHtmlPlugin(),
+    createHtmlPlugin({ inject: { data: { ...mdi } } }),
     VitePWA({
       manifest,
       includeManifestIcons: false,
