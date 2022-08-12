08a102e215251e05a858293b8e9a3df8963c2509
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-08-12 19:10:19 -0400

    refactor: ♻️ remove vite "base"

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,17 @@ import { VitePWA } from "vite-plugin-pwa";
 import * as mdi from "@mdi/js";
 import { manifest } from "./manifest";
 
-const BASE_URL = "https://othello-rust.web.app/";
-
 export default defineConfig({
-  base: BASE_URL,
   plugins: [
     createHtmlPlugin({
-      inject: { data: { ...mdi, DESCRIPTION: manifest.description, TITLE: manifest.name, BASE_URL } },
+      inject: {
+        data: {
+          ...mdi,
+          DESCRIPTION: manifest.description,
+          TITLE: manifest.name,
+          BASE_URL: "https://othello-rust.web.app",
+        },
+      },
     }),
     VitePWA({
       manifest,
