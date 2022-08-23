91def6d78afc3d4d41c122fbf2822009e9310185
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-08-23 18:21:14 -0400

    feat: ✨ add IARC rating id

--- manifest.ts
+++ manifest.ts
@@ -8,6 +8,7 @@ export const manifest: Partial<ManifestOptions> = {
   background_color: "#121212",
   theme_color: "#121212",
   categories: ["game"],
+  iarc_rating_id: "b5c3da86-e1a0-4f97-86b6-a3f6246c90ff",
   icons: getIcons(),
   screenshots: getScreenshots(),
 };
