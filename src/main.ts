77fb5393a2d1cfce66413cc70dd7e70dde1a3503
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2023-05-02 15:55:29 -0400

    refactor: ♻️ polyfill dialogs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as Comlink from "comlink";
 import { htmlPlayer } from "./game/html-player";
 import "./settings";
 import type { PlayFunction } from "./worker";
+import dialogPolyfill from "dialog-polyfill";
 
 const worker = new Worker(new URL("./worker.ts", import.meta.url), { type: "module" });
 
@@ -16,3 +17,7 @@ play(player).then(() => {
   });
   newGameButton.classList.add("show");
 });
+
+for (const dialog of document.querySelectorAll("dialog")) {
+  dialogPolyfill.registerDialog(dialog);
+}
