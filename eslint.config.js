4c05249bb6a5deeb45535ec4d015343bf47faba7
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-06-17 20:49:21 -0400

    ci: 👷 use more strict eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,17 @@
 // @ts-check
 
-import eslint from "@eslint/js";
+import js from "@eslint/js";
 import ts from "typescript-eslint";
 
 export default ts.config(
-  eslint.configs.recommended,
-  ...ts.configs.recommended,
+  js.configs.recommended,
+  ...ts.configs.strict,
+  ...ts.configs.stylistic,
   { ignores: ["dist/", "rust-othello/"] },
   {
     rules: {
       "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
       "no-constant-condition": ["error", { checkLoops: false }],
     },
   },
