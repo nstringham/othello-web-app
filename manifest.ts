8fabd340ab1cb51b0ae4287974bcc00c774d2c76
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-06-18 21:36:48 -0400

    chore: 🔖 increment version number

--- manifest.ts
+++ manifest.ts
@@ -32,7 +32,7 @@ export const manifest: WebAppManifest & { b2g_features: object } = {
     },
   ],
   b2g_features: {
-    version: "3.2.1",
+    version: "3.3.0",
     type: "web",
     subtitle: "Play Othello against an AI opponent",
     permissions: {},
