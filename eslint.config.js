3d0d85b9dbbbb16dc19f7d09682845de32c552e7
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2026-01-12 13:38:04 -0500

    refactor: 🔧 use defineConfig from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 // @ts-check
 
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import ts from "typescript-eslint";
 
-export default ts.config(
+export default defineConfig(
   js.configs.recommended,
   ...ts.configs.strict,
   ...ts.configs.stylistic,
